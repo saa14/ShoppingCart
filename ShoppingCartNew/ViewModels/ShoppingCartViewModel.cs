02fc470796944332434cdc5ee0aafec058507081
saa14 <saagar.minocha@outlook.com>
2015-08-26 15:52:08 +0530

    Cart ViewModels added

---
+++ ShoppingCartNew/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingCartNew.Models;
+
+namespace ShoppingCartNew.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
